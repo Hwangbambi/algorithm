26cb34fbbd2448a6c5e3f0c5f8a5652460de78ab
Soojeong Hwang <100077017+Hwangbambi@users.noreply.github.com>
2023-01-01 23:30:13 +0900

    JadenCase 문자열 만들기

---
+++ programmers_Lv2/Today20230101.java
@@ -0,0 +1,18 @@
+package codingTest;
+
+public class Today20230101 {
+    //Q. JadenCase 문자열 만들기
+    //s	return
+    //"3people unFollowed me" -> "3people Unfollowed Me"
+    //"for the last week"-> "For The Last Week"
+
+    public static String solution(String s) {
+        String answer = "";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Today20230101.solution("3people unfollowed me");
+    }
+
+}
